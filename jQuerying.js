18ccf8c06516a6a8d2c4dec6f5102c50041e3708
=Andy Gross <agross@colorado.edu>
2015-10-06 13:02:34 -0600

    mostly there

--- jQuerying.js
+++ jQuerying.js
@@ -1,1 +1,64 @@
+$(document).ready(function(){
 
+//1)
+$("#hover").mouseenter(function() {
+  $(this).css("background-color", "red");
+});
+
+$("#hover").click(function() {
+  $(this).toggleClass("green");
+});
+
+//2)
+$(".hideMe").click(function() {
+  $(this).remove();
+});
+
+//3)
+ $(".changeColor").click(function() {
+   $("li:nth-child(3)").css("color", "blue");
+ });
+//4)
+ $("#removeLi").click(function() {
+   $(".only").remove();
+ });
+//5:  keeps appending)
+$("ul li:nth-child(4)").click(function(){
+  $(this).append("<li>Another Item</li>");
+});
+
+//6)
+$("ul li").click(function(){
+ $("li").last().css("font-size","40px");
+ $("li:lt(4)").hide("slow");
+});
+
+//7)
+
+  $(".cute").click(function() {
+   $(".cute").clone().appendTo(".clones");
+  });
+
+//8
+$("*").dblclick(function(){
+  $("#makeSquare").css({"width":"300px", "height":"300px", "border-radius": "0px"});
+});
+
+//9
+$("#dontClick").click(function() {
+alert("Why you clickin that button?");
+});
+
+
+//10
+$(".swatch.black").click(function() {
+  $("body").addClass("black").removeClass("wood","chaos");
+});
+$(".swatch.wood").click(function() {
+  $("body").addClass("wood").removeClass("black","chaos");
+});
+$(".swatch.chaos").click(function() {
+  $("body").addClass("chaos").removeClass("wood","black");
+});
+
+});
--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+$(document).ready(function(){
+
 // EXERCISE THE JQUERY
 
 // We'll be using the html in index.html for the following jQuery exercises.
@@ -5,18 +7,28 @@
 
 // 1. Hide all of the paragraphs on the index.html page.
 
+//$("p").hide();
 
 // 2. Hide all of the h1's on the page.
 
+//$("h2").hide();
 
 // 3. Hide all of the elements in the document.
 
+//not sure what elements means?
 
 // 4. Hide all links on the page
 
+//$("a").hide();
 
 // 5. Hide all of the odd table rows (Austin, Colin, & Jessica's rows)
 
-
+//$("tr:odd").hide();
 
 // 5. Hide all paragraphs when they're clicked.
+
+$("p").click(function() {
+$("p").hide();
+});
+
+});
